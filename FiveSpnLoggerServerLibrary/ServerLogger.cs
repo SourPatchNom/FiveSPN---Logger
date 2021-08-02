71448bc7a5c201a02d9375c8492259cc5bc2b8dc
SourPatchNom <11154163+SourPatchNom@users.noreply.github.com>
2021-08-02 04:24:01 -0500

    refactor(core):core work

---
+++ FiveSpnLoggerServerLibrary/ServerLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FiveSpnLoggerClientLibrary.Classes;
+using FiveSpnLoggerClientLibrary.Enums;
+
+namespace FiveSpnLoggerClientLibrary
+{
+    public class ServerLogger
+    {
+        private static readonly ServerLogger _instance = new ServerLogger();
+        public static ServerLogger Instance
+        {
+            get { return _instance; }
+        }
+
+        static ServerLogger()
+        {
+
+        }
+        private ServerLogger()
+        {
+            SendServerLogMessage(new LogMessage("FiveSPN - Logger",LogMessageSeverity.Info,"Logger initialized."));
+        }
+
+        public static void SendServerLogMessage(LogMessage logMessage)
+        {
+            string messageCombined = $"{DateTime.Now,-19} [{logMessage.Severity,8}] {logMessage.Source}: {logMessage.Message}";
+            Console.WriteLine(messageCombined);
+            // if (logMessage.Severity == LogMessageSeverity.Error || logMessage.Severity == LogMessageSeverity.Critical)
+            // {
+            //     BaseScript.TriggerServerEvent("ServerBasics:ClientLogMessage", API.PlayerId(), messageCombined);
+            // }
+        }
+    }
+}
