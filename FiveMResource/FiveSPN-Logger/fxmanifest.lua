824bbec2d51f2d85e0add9f8a2e4be79e62f778e
SourPatchNom <nom@itsthenom.com>
2021-08-06 21:22:08 -0500

    refactor(manifest):update information in manifest.

--- FiveMResource/FiveSPN-Logger/fxmanifest.lua
+++ FiveMResource/FiveSPN-Logger/fxmanifest.lua
@@ -6,12 +6,6 @@ author 'SourPatchNom'
 version 'v1.0'
 url 'https://itsthenom.com'
 
-utc_offset = '0' -- The offset in hours from UTC time for the server
-run_speed_modifier = '1.1999' -- Run speed is multiplied times this number as float x.xxx
-discord_rp_key = '700338921246294026'
-discord_rp_asset = 'sp512'
-discord_rp_asset_small = 'sp512'
-
 games { 'gta5' }
 
 server_script {
