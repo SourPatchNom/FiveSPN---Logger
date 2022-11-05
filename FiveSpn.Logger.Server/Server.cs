9937e049a9de725073b403916bd492d69db9c067
SourPatchNom <nom@itsthenom.com>
2022-11-05 11:17:06 -0500

    feat(log):add physical log generation

--- FiveSpn.Logger.Server/Server.cs
+++ FiveSpn.Logger.Server/Server.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using FiveSpn.Logger.Library.Classes;
 using FiveSpn.Logger.Library.Enums;
 
@@ -7,31 +10,61 @@ namespace FiveSpn.Logger.Server
 {
     public class Service : BaseScript
     {
+        private readonly LogMessageSeverity _severity = LogMessageSeverity.Debug;
+        private readonly string _logPath = API.GetResourcePath(API.GetCurrentResourceName()) + "/FiveSpnLog.txt";
+        
         public Service()
         {
-            SendServerLogMessage(new LogMessage("Server Logger",LogMessageSeverity.Info,"New resource logger initialized."));
+            try
+            {
+                File.Delete(_logPath);
+                File.Create(_logPath);
+            }
+            catch (Exception e)
+            {
+                WriteMessageToConsole(LogMessageSeverity.Critical,"Logging File Error!\n"+e.Message);
+            }
+            
+            ProcessServerLogMessage(new LogMessage("Server Logger", LogMessageSeverity.Info,"New resource logger initializing."));
             EventHandlers["FiveSPN-ServerLogToServer"] += new Action<string, int, string>(HandleServerLogMessage);
             EventHandlers["FiveSPN-ClientLogToServer"] += new Action<Player, string, int, string>(HandlePlayerLogMessage);
         }
 
         private void HandlePlayerLogMessage(Player arg1, string arg2, int arg3, string arg4)
         {
-            SendServerLogMessage(new LogMessage(arg1.Name +" " + arg2, (LogMessageSeverity)arg3, arg4));
+            ProcessServerLogMessage(new LogMessage(arg1.Name +" " + arg2, (LogMessageSeverity)arg3, arg4));
         }
 
         private void HandleServerLogMessage(string arg1, int arg2, string arg3)
         {
-            SendServerLogMessage(new LogMessage(arg1, (LogMessageSeverity)arg2, arg3));
+            ProcessServerLogMessage(new LogMessage(arg1, (LogMessageSeverity)arg2, arg3));
         }
 
-        public static void SendServerLogMessage(LogMessage logMessage)
+        private void ProcessServerLogMessage(LogMessage logMessage)
         {
-            //string messageCombined = $"{DateTime.Now,-19} [{logMessage.Severity,8}] {logMessage.Source}: {logMessage.Message}";
-            string messageCombined = $"FiveSpn>[{logMessage.Source,20}][{logMessage.Severity,8}] {DateTime.Now,-19} : {logMessage.Message}";
+            if (logMessage.Severity > _severity) return;
+            var messageCombined = $"FiveSpn>[{logMessage.Source,20}][{logMessage.Severity,8}] {DateTime.Now,-19} : {logMessage.Message}";
+            WriteMessageToLogFile(messageCombined);
             WriteMessageToConsole(logMessage.Severity, messageCombined);
         }
-        
-        private static void WriteMessageToConsole(LogMessageSeverity severity, string messageCombined)
+
+        private void WriteMessageToLogFile(string messageCombined)
+        {
+            try
+            {
+                using (var writer = File.AppendText(_logPath))
+                {
+                    writer.WriteLine(messageCombined);
+                    writer.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                WriteMessageToConsole(LogMessageSeverity.Critical,"Logging File Error!\n"+e.Message);
+            }
+        }
+
+        private void WriteMessageToConsole(LogMessageSeverity severity, string messageCombined)
         {
             try
             {
@@ -48,8 +81,10 @@ namespace FiveSpn.Logger.Server
                         Console.ForegroundColor = ConsoleColor.DarkGreen;
                         break;
                     case LogMessageSeverity.Verbose:
-                    case LogMessageSeverity.Debug:
                         Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    case LogMessageSeverity.Debug:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -61,7 +96,7 @@ namespace FiveSpn.Logger.Server
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 //TODO loop?
-                SendServerLogMessage(new LogMessage("Server Logger",LogMessageSeverity.Error,"Exception thrown while attempting to process log message from client.\n"+e.Message));
+                ProcessServerLogMessage(new LogMessage("Server Logger",LogMessageSeverity.Error,"Exception thrown while attempting to process log message from client.\n"+e.Message));
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
